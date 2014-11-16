60d4dfe973a2a96048fb0802b02d0d25339e6d42
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-11-15 22:25:24 -0500

    Add basic re-sort on click

--- js/sexurl.js
+++ js/sexurl.js
@@ -1,23 +1,24 @@
 $(function(){
   var $c = $('#blocks'),
-      $blocks = parseBlocks(data.blocks, 1);
+      $blocks = parseBlocks(data.blocks, 1, null);
 
   $c.append($blocks);
   $(document).on('click', '.block', onBlockClick);
 })
 
-function parseBlocks(blocks, level) {
+function parseBlocks(blocks, level, parent) {
   var $blocks = [];
 
   $.each(blocks, function(i){
     var block = blocks[i];
         $block = $('<div class="block level-' + level + '">' + block.title + '</div>');
 
+    block.parent = parent;
     $block.data('block', block);
     $blocks.push($block);
 
     if(block.childs){
-      $blocks = $blocks.concat(parseBlocks(block.childs, level + 1));
+      $blocks = $blocks.concat(parseBlocks(block.childs, level + 1, block));
     }
   })
 
@@ -27,11 +28,28 @@ function parseBlocks(blocks, level) {
 function onBlockClick(){
   var $e = $(this),
       $activeBlock = $('#active-block'),
-      data = $e.data('block'),
+      block = $e.data('block'),
       $content = '';
 
-  $content += '<h2>' + data.title + '</h2>';
-  $content += '<p>' + data.text + '</p>';
+  $content += '<h2>' + block.title + '</h2>';
+  $content += '<p>' + block.text + '</p>';
 
   $activeBlock.html($content);
+  orderBlocks(block);
+}
+
+function orderBlocks(current){
+  var $c = $('#blocks');
+
+  // Sorting only root nodes
+  if(current.parent) {
+    current = current.parent;
+  }
+
+  data.blocks.sort(function(a,b) {
+    return (current === a) ? -1 : 1;
+  });
+
+  $blocks = parseBlocks(data.blocks, 1, null);
+  $c.html($blocks);
 }
