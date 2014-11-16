e052f0a7fc098f3f6211768fdf562f975a4e18b3
David Lapointe Gilbert <dalap114@dalap114-3.local>
2014-11-15 23:00:36 -0500

    Ajout des données déjà présentes dans le drive présentement

--- js/data.js
+++ js/data.js
@@ -24,6 +24,119 @@ var data = {
           tags: '#vagin #iritation'
         }
       ]
+    },
+    {
+      title: 'Comportement',
+      slug: 'comp',
+      text: 'Qui, quoi, comment, pourquoi?',
+      childs: [
+        {
+          title: 'Comment embrasser?',
+          slug: 'comp-embrasser',
+          text: "Il est difficile de dire comment faire pour embrasser quelqu’un puisqu’il n’y a pas de technique particulière. En fait, embrasser ça s'apprendra avec ton chum ou ta blonde!",
+          tags: '#french #baiser #langue #bave #salive #microbe'
+        },
+        {
+          title: 'C’est quoi les préliminaires ?',
+          slug: 'comp-prelims',
+          text: "C’est des échanges de tendresse et de caresses qui précède l’acte sexuel pour préparer le terrain! Les femmes ont en général besoin de plus de temps pour préparer leur appareil sexuel à un acte sexuel (humidifier leur vagin). Il est donc important de s’assurer que les deux partenaires sont prêt avant de passer à l’acte.",
+          tags: '#tendre #caresse #etape #vagin'
+        }
+      ]
+    },
+    {
+      title: 'Baiser / fourrer / faire l’amour',
+      slug: 'baiser',
+      text: 'Pas le genre de baiser que matante Ginette te fait à Noël',
+      childs: [
+        {
+          title: 'À quoi me servent les préliminaires ?',
+          slug: 'baiser-prelims',
+          text: "Ça sert d’abord à préparer tes organes qui vont s’humidifier pour faciliter l’acte sexuel, mais également à préparer ton esprit ou celui de ta partenaire pour que le tout soit agréable.",
+          tags: '#etape #vagin'
+        },
+        {
+          title: 'Quand est-ce que je sais que c’est le bon moment ?',
+          slug: 'baiser-moment',
+          text: "Il n’y a que toi qui peux décider du bon moment pour ta première (mais aussi toute relation sexuelle). Il n’est jamais trop tard pour refuser une relation sexuelle, même si tu es rendu nu et que tout le monde est excité, n’hésites pas à changer d’idée et remettre tes pantalons.",
+          tags: '#attendre #temps #couple #amour #inquiétude #peur #débandade #pression #envie #préliminaire #absencedesparents #performance #feeling #refus #viol #non'
+        },
+        {
+          title: 'Qui enlève le linge à qui ?',
+          slug: 'baiser-linge',
+          text: "Tu devrais tout simplement le demander à ton partenaire! Mais non, ce n’est pas nécessairement une pratique commune ex. enlever le linge de la fille comme on peut voir dans les films.",
+          tags: '#etape #gene #vetement #toutnu #nu #apoil'
+        }
+      ]
+    },
+    {
+      title: 'Porn star',
+      slug: 'pornstar',
+      text: "Un peu plus loin qu'Hollywood",
+      childs: [
+        {
+          title: 'J’écoute trop de porno, c’est un problème ?',
+          slug: 'pornstar-porno',
+          text: "Dis toi que c’est comme dans tout, trop de n’importe quoi c’est pas bon pour personne. Plus sérieusement, n’oublies pas que la pornographie c’est du cinéma et qu’au cinéma ce sont des acteurs pros qui font leur travail. Tu peux t’inspirer de la pornographie, mais assures toi toujours du consentement de ta partenaire avant d’essayer une nouvelle pratique sexuelle.",
+          tags: '#porno'
+        },
+        {
+          title: 'La porno quoi en penser ?',
+          slug: 'pornstar-penser',
+          text: "La pornographie n’est pas une représentation de la réalité, ça n’a rien à voir avec ce qu’il se passe dans le lit de tous et chacun. ",
+          tags: '#pression #realité #femmeobjet #performance #peur'
+        },
+        {
+          title: 'Lumière, moteur, action ?',
+          slug: 'pornstar-lumiere',
+          text: "Le mieux est toujours d’en discuter avec ton/ ta partenaire pour décider de laisser la lumière ouverte ou non. C’est un choix personnel et c’est tout à fait normal qu’il soit plus gênant de faire l’amour avec la lumière ouverte et ce n’est pas absolument nécessaire, on peut trouver le chemin sans!",
+          tags: '#pression #performance #gene'
+        },
+        {
+          title: 'Dois-je avaler ?',
+          slug: 'pornstar-avaler',
+          text: "Pas du tout, c’est à chacun de choisir s’il veut avaler quoi que ce soit (incluant du sperme). Il est à noter qu’il n’y a aucun danger à recevoir du sperme sur toi ou à l’avaler, mais ne te sens pas obliger de le faire si tu n’y a pas goût.",
+          tags: '#sperm #gout #avaler #bouche'
+        }
+      ]
+    },
+    {
+      title: 'Pipe à bouche, avec ou sans main et substance gélatineuse au goût douteux',
+      slug: 'pipe',
+      text: 'Le mettre dans sa bouche!',
+      childs: [
+        {
+          title: 'Qu’est-ce qu’une fellation ?',
+          slug: 'pipe-fellation',
+          text: "La fellation est une des pratiques sexuelles qui consiste à stimuler le pénis de ton partenaire avec la bouche, les lèvres et la langue. Elle peut être pratiquée soit comme un préliminaire soit pour le conduire jusqu'à l’orgasme (éjaculation).",
+          tags: '#pipe #bouche #penis '
+        },
+        {
+          title: 'Comment faire une fellation ?',
+          slug: 'pipe-comment',
+          text: "Si vous êtes débutante en matière de fellation, n'hésitez pas à masser son pénis en guise de préliminaire. En utilisant tes doigts, tu pourras découvrir ses réactions et la forme de son sexe : vous ressentirez ainsi moins d'appréhension au moment de le prendre en bouche. Soyez aux aguets du moindre changement de posture. S'il se rétracte, s'il a un mouvement de recul, s'il rate une respiration, c'est que la caresse est désagréable. Par contre s'il se cabre, gémit ou halète, tu es sur la bonne voie.",
+          tags: '#taillerunepipe #pipe #sucer #suce #suction #main #bouche #herpèsbucale #dents'
+        }
+      ]
+    },
+    {
+      title: 'Spécialiste',
+      slug: 'specialiste',
+      text: "Tu crois être le spécialiste?",
+      childs: [
+        {
+          title: 'Pourquoi le pénis devient dur ?',
+          slug: 'specialiste-dur',
+          text: "C’est une érection. Ce phénomène physiologique fait que le pénis devient ferme et élargi. L’érection est le résultat de l’excitation sexuelle ou de l’attirance sexuelle.
+
+Après l’orgasme, le pénis perd sa fermeté!
+
+En état de stress ou de pression il arrive que l’homme perde son érection. Ne t’en fait pas, discute ou fais des préliminaires et ça reviendra!
+
+Les femmes aussi ont des érections mais clitoridienne!",
+          tags: '#erection #bande #debander #sang #muscle #organe #excitation #biologique '
+        }
+      ]
     }
   ]
 }
