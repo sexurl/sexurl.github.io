442980167a2e1294e5cc411a7bc5f95dc4e62b0f
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-11-15 21:29:51 -0500

    Add basic data structure

---
+++ js/data.js
@@ -0,0 +1,29 @@
+var data = {
+  blocks: [
+    {
+      title: 'Ma première fois',
+      slug: 'mpf',
+      text: 'La première fois, vaut mieux être prêt!',
+      childs: [
+        {
+          title: 'Est-ce que ça fait mal?',
+          slug: 'mpf-mal',
+          text: 'Il se peut que la première relation soit douloureuse, cela dépend de chaque personne et c’est à cause de la fracture de l’hymen, un petit bout de peau qui protège votre dedans avant votre première relation.',
+          tags: '#douleur #vagin #hymen #pénétration #anal #prostate #déchirer'
+        },
+        {
+          title: 'Le sport peut briser mon hymen?',
+          slug: 'vagin-sport',
+          text: 'L’équitation, le vélo, les autres sports peuvent effectivement déformer l’hymen avant votre première relation, mais ce n’est pas uniquement le sport. Chez l’humain l’hymen se déforme naturellement au cours de l’adolescence et bien que cela peut se produire lors de votre première relation, cela peut aussi arriver n’importe quand.',
+          tags: '#hymen'
+        },
+        {
+          title: 'Mon vagin n’est pas mouillé lors de la relation sexuelle, est-ce normal?',
+          slug: 'vagin-humide',
+          text: 'Il ne faut pas paniquer, c’est tout à fait normal pour différentes personnes d’avoir un niveau de lubrification naturelle différent. Il ne faut pas se gêner pour utiliser des produits adapté acheté à la pharmacie (ne pas utiliser de l’huile d’olive!) pour rendre vos relations plus agréables.',
+          tags: '#vagin #iritation'
+        }
+      ]
+    }
+  ]
+}
--- js/sexurl.js
+++ js/sexurl.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+$(function(){
+  var $c = $('#content'),
+      $blocks = parseBlocks(data.blocks, 1);
+
+  $c.append($blocks);
+})
+
+
+function parseBlocks(blocks, level) {
+  var $blocks = [];
+
+  $.each(blocks, function(i){
+    var block = blocks[i];
+        $block = $('<div class="block level-' + level + '">' + block.title + '</div>');
+
+    $block.data('block', block);
+    $blocks.push($block);
+
+    if(block.childs){
+      $blocks = $blocks.concat(parseBlocks(block.childs, level + 1));
+    }
+  })
+
+  return $blocks;
+}
